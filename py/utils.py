bb930c57899c7876f6d6692a2de6254c582d9e0e
Matthew Carbone <x94carbone@gmail.com>
2021-03-22 15:42:44 -0400

    Add listdir full path function to utils

--- py/utils.py
+++ py/utils.py
@@ -9,6 +9,10 @@ __status__ = "Prototype"
 import os
 
 
+def listdir_fp(d):
+    return [os.path.join(d, f) for f in os.listdir(d)]
+
+
 def get_cache():
     cache = os.environ.get("HDSPIN_CACHE_DIR")
     assert cache is not None
