f76bb75cf20883fb030cc6f111791f7060f3b091
Matthew Carbone <x94carbone@gmail.com>
2023-06-13 08:37:42 -0400

    Add state manipulation library for arbitrary precision

--- lib/state_manipulation.h
+++
@@ -1,3 +0,0 @@
-#include "ArbitraryPrecision/ap/ap.hpp"
-
-std::vector<ap_uint<10000>> get_neighbors(ap_uint<10000>, int);
---
+++ lib/state_manipulation/state_manipulation.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include "state_manipulation.h"
+#include "ArbitraryPrecision/ap/ap.hpp"
+
+ap_uint<PRECISON> _arbitrary_precision_integer_pow(
+    const int base, const int exponent)
+{
+    if (exponent == 0)
+    {
+        ap_uint<PRECISON> val = 1;
+        return val;   
+    }
+    ap_uint<PRECISON> val = base;
+    for (int ii=0; ii<exponent - 1; ii++)
+    {
+        val *= base;
+    }
+    return val;
+}
+
+namespace state_manipulation
+{
+    std::vector<ap_uint<PRECISON>> get_neighbors(ap_uint<PRECISON> n,
+        int bitLength)
+    {
+        std::vector<ap_uint<PRECISON>> result;
+        
+        for (int b = 0; b < bitLength; b++)
+        {
+            ap_uint<PRECISON> one = 1;
+            result.push_back(n ^ (one << b));
+        }
+        return result;
+    }
+
+    void arbitrary_precision_integer_from_int_array_(
+        const int *config, const int N, ap_uint<PRECISON> &res)
+    {
+        res = 0;
+        for (int ii=N-1; ii>=0; ii--)
+        {
+            const ap_uint<PRECISON> config_val = config[N - ii - 1];
+            const ap_uint<PRECISON> power_val =  _arbitrary_precision_integer_pow(2, ii);
+            res += config_val * power_val;
+        }
+    }
+
+    void int_array_from_arbitrary_precision_integer_(
+        int *config, const int N, ap_uint<PRECISON> integer)
+    {
+        for (int ii=0; ii<N; ii++)
+        {
+            ap_uint<PRECISON> remainder = integer % 2;
+            integer = integer / 2;
+            config[N - ii - 1] = int(remainder);
+        }
+    }
+}
+
