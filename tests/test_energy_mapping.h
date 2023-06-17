a69fbed4af9e3cff5820ec59c4afc9eedc3bbb11
Matthew Carbone <x94carbone@gmail.com>
2023-06-17 19:46:33 -0400

    Add emap tests to testing suite

--- tests/test_energy_mapping.h
+++ tests/test_energy_mapping.h
@@ -1,5 +1,6 @@
 #include "energy_mapping.h"
 #include "utils.h"
+#include "utils_testing_suite.h"
 
 bool _test_energy_mapping_sampling_EREM_given_beta_critical(const double beta_critical)
 {
@@ -10,15 +11,54 @@ bool _test_energy_mapping_sampling_EREM_given_beta_critical(const double beta_cr
     sp.seed = 1234;
     EnergyMapping emap = EnergyMapping(sp);
     const double mean = -1.0 / sp.beta_critical;
-    double v = 0.0;
+    const double variance = 1.0 / sp.beta_critical / sp.beta_critical;
     const int N = 1000000;
-    const double eps = 0.01;
+    const double eps = 0.1;
+    std::vector<double> v;
     for (int ii=0; ii<N; ii++)
     {
-        v += emap.sample_energy();
+        v.push_back(emap.sample_energy());
     }
-    v /= double(N);
-    // std::cout << v << " " << mean << std::endl;
-    if ((v < mean + eps) && (v > mean - eps)){return true;}
-    return false;
+
+    const double num_mean = _mean_vector(v);
+    const double num_var = _variance_vector(v);
+
+    // std::cout << num_mean << " " << mean << std::endl;
+    // std::cout << num_var << " " << variance << std::endl; 
+
+    if ((num_mean > mean + eps) || (num_mean < mean - eps)){return false;}    
+    if ((num_var > variance + eps) || (num_var < variance - eps)){return false;}
+
+    return true;
+}
+
+
+bool _test_energy_mapping_sampling_REM_given_N_spins(const int N_spins)
+{
+    parameters::SimulationParameters sp;
+    sp.landscape = "REM";
+    sp.N_spins = N_spins;
+    sp.use_manual_seed = true;
+    sp.seed = 4567;
+    EnergyMapping emap = EnergyMapping(sp);
+    const double mean = 0.0;
+    const double variance = N_spins;
+    const int N = 10000000;
+    const double eps = 0.1;
+    std::vector<double> v;
+    for (int ii=0; ii<N; ii++)
+    {
+        v.push_back(emap.sample_energy());
+    }
+
+    const double num_mean = _mean_vector(v);
+    const double num_var = _variance_vector(v);
+
+    // std::cout << num_mean << " " << mean << std::endl;
+    // std::cout << num_var << " " << variance << std::endl;
+
+    if ((num_mean > mean + eps) || (num_mean < mean - eps)){return false;}    
+    if ((num_var > variance + eps) || (num_var < variance - eps)){return false;}
+
+    return true;
 }
---
+++ tests/utils_testing_suite.h
@@ -0,0 +1,15 @@
+#include <vector>
+#include <numeric>
+
+double _mean_vector(const std::vector<double> v)
+{
+    const double sum = std::accumulate(v.begin(), v.end(), 0.0);
+    return sum / v.size();
+}
+
+double _variance_vector(const std::vector<double> v)
+{
+    const double mean = _mean_vector(v);
+    const double sq_sum = std::inner_product(v.begin(), v.end(), v.begin(), 0.0);
+    return sq_sum / v.size() - mean * mean;
+}
