c43b22cd8982c0b9c9a94691ec85ef48b7a88df9
Matthew Carbone <x94carbone@gmail.com>
2023-11-22 13:39:52 -0500

    Document main utils

--- inc/main_utils.h
+++ inc/main_utils.h
@@ -7,45 +7,70 @@
 #include "obs2.h"
 
 
-// TODO: add documentation
 
+/**
+ * The main_utils namespace provides an API for the entrypiont in main.cpp.
+ * This way everything can be properly scoped and organized. main.cpp will only
+ * call functions from this namespace.
+ */
 namespace main_utils
 {
 
 
 /**
- * @brief [brief description]
- * @details [long description]
- * @return [description]
+ * @brief Completes the CLI-provided parameters
+ * @details The user has full control over almost every part of the simulation,
+ * but can optionally not provide explicit arguments for all of them. Additionally,
+ * some parameters are explicit functions of what the user provides. This method
+ * modifies the simulation parameters in-place.
+ * 
+ * @param p Simulation parameters
  */
 void update_parameters_(utils::SimulationParameters* p);
 
 
+/**
+ * @brief Saves and logs the config file
+ * @details Given a SimulationParameters struct, this first converts the
+ * object to json format, then logs it (prints to std::cout), then saves it as
+ * config.json in the working directory.
+ * 
+ * @param p Simulation parameters
+ */
 void save_and_log_config(const utils::SimulationParameters p);
 
 /**
- * @brief [brief description]
- * @details [long description]
+ * @brief Initializes directories and the grid utility files
+ * @details hdspin requires an energy and "pi" grid. This function creates the
+ * data and grids directories, then populates the grid directory with the
+ * aforementioned files.
  * 
- * @param p [description]
+ * @param p Simulation parameters
  */
 void initialize_grids_and_directories(const utils::SimulationParameters p);
 
 
 /**
- * @brief [brief description]
- * @details [long description]
+ * @brief Given "auto" as a dynamics type, automatically determines which
+ * dynamics will be faster (gillespie or standard)
+ * @details This is an MPI-aware function which uses all available tasks to get
+ * some lightweight statistics on which simulation dynamics will be faster.
+ * Specifically, it calculates the average walltime/simulation time (this is
+ * necessary since gillespie can, by design, sometimes overshoot the maximum
+ * simulation time). The faster simulator is then set in the simulation
+ * parameters in-place.
  * 
- * @param params [description]
+ * @param p Simulation parameters
  */
 void auto_determine_dynamics_(utils::SimulationParameters* params);
 
 
 /**
- * @brief [brief description]
- * @details [long description]
+ * @brief Executes the simulation
+ * @details Utilizes a process pool: a single "master" controller dynamically
+ * allocates tasks to workers until the total number of simulations is complete.
  * 
- * @param params [description]
+ * @param p Simulation parameters
  */
 void execute_process_pool(const utils::SimulationParameters params);
 
